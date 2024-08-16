30b7a3ded77889a696359588782073459d3457a0
imAshutoshGupta <aguptaworkspace@gmail.com>
2024-08-16 16:06:23 +0530

    temp carousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import { Carousel, Container } from 'react-bootstrap';
+import { Card, Carousel, Col, Container, Row } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Carousel.css';
+import beyours from '../assets/beyours.jpeg'
+import leaf from '../assets/leaf.png'
+import snitch from '../assets/snitch.png'
 
 const carouselItems = [
-  { id: 1, src: 'https://via.placeholder.com/600x200', alt: 'Slide 1', caption: 'Slide 1 Caption' },
-  { id: 2, src: 'https://via.placeholder.com/600x200', alt: 'Slide 2', caption: 'Slide 2 Caption' },
-  { id: 3, src: 'https://via.placeholder.com/600x200', alt: 'Slide 3', caption: 'Slide 3 Caption' },
+  { id: 1, src: beyours, alt: 'beyours', caption: 'XIRCLS is disrupting the marketing industry through collaborative marketing,while helping all brands to increase their reach & sales.', name: 'Nilesh Karnani & Ashish Baheti, Co-founders of BeYours' },
+  { id: 2, src: leaf, alt: 'leaf 2', caption: 'XIRCLS is disrupting the marketing industry through collaborative marketing,while helping all brands to increase their reach & sales.', name: 'Nilesh Karnani & Ashish Baheti, Co-founders of BeYours' },
+  { id: 3, src: snitch, alt: 'snitch', caption: 'XIRCLS is disrupting the marketing industry through collaborative marketing,while helping all brands to increase their reach & sales.', name: 'Nilesh Karnani & Ashish Baheti, Co-founders of BeYours' },
 ];
 
 const ResponsiveCarousel = () => (
@@ -21,14 +24,26 @@ const ResponsiveCarousel = () => (
       <Carousel className="my-carousel">
         {carouselItems.map(item => (
           <Carousel.Item key={item.id}>
-            <img
-              className="d-block w-100"
-              src={item.src}
-              alt={item.alt}
-            />
-            <Carousel.Caption>
-              <h3>{item.caption}</h3>
-            </Carousel.Caption>
+            <Row>
+              <Col className='col-4'>
+                <Card className="review-card">
+                  <div className="image-container">
+                    <img src={item.src} alt={beyours} width={100} height={100} className="profile-image" />
+                  </div>
+                  <Card.Body>
+                    <Card.Text>{item.caption}</Card.Text>
+                    <Card.Title>{item.name}</Card.Title>
+                  </Card.Body>
+                </Card>
+              </Col>
+              <Col className='col-4'>
+                <h1>hello</h1>
+              </Col>
+              <Col className='col-4'>
+                <h1>hello</h1>
+              </Col>
+            </Row>
+
           </Carousel.Item>
         ))}
       </Carousel>
